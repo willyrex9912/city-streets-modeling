ba3a45e3d385ea1c07b07a5b6acfe6b9da4bb523
willyrex9912 <willyjessiel99@gmail.com>
2024-05-02 19:55:34 -0600

    Updating graph

--- util/grapher.py
+++ util/grapher.py
@@ -14,26 +14,28 @@ class Grapher:
     def graph(cross_streets_map: Dict[int, CrossStreets], street_map: Dict[int, Street], individual: Individual | None):
         dot = Digraph()
         dot.attr(rankdir='LR')
-        for key, cross in cross_streets_map.items():
+        for key in cross_streets_map.keys():
             name = str(key)
             label = "Cross " + name
-            data_list: List[StreetData] = cross.get_streets_by_direction(Direction.START)
-            if len(data_list) > 0:
-                label += "\nOut = [ "
-                for data in data_list:
-                    label += str(data.street_id) + ":" + str(individual.genes[data.street_id].start_percentage) + "% "
-                label += "]"
             dot.node(name, label)
         for key, street in street_map.items():
             name = str(key)
             label = "Street " + name
             if street.start_cross_id is None:
                 dot.node('Dummy ' + name, style='invisible')
+                label += f"\nPercentage: 100%"
+                label += f"\nTotal: {street.capacity}"
                 dot.edge('Dummy ' + name, str(street.end_cross_id), label=label)
             elif street.end_cross_id is None:
                 dot.node('Dummy ' + name, style='invisible')
+                label += f"\nPercentage: {individual.genes[street.id].start_percentage}%"
+                label += f"\nTotal: {individual.genes[street.id].start_number}"
+                label += f"\nMin: {cross_streets_map[street.start_cross_id].street_map[street.id].min_percentage}%"
                 dot.edge(str(street.start_cross_id), 'Dummy ' + name, label=label)
             else:
+                label += f"\nPercentage: {individual.genes[street.id].start_percentage}%"
+                label += f"\nTotal: {individual.genes[street.id].start_number}"
+                label += f"\nMin: {cross_streets_map[street.start_cross_id].street_map[street.id].min_percentage}%"
                 dot.edge(str(street.start_cross_id), str(street.end_cross_id), label=label)
         caption = f"Total inputs: {individual.total_inputs}\nTotal outputs: {individual.total_outputs}\n"
         caption += f"Total efficiency: {individual.aptitude}%"
